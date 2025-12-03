2fcb2dd21e003d89a949da3eab143e733d037995
NoLimitsChenrui <liuchenrui2@gmail.com>
2025-12-03 15:33:07 -0500

    Complete 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => total += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => total *= num);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num===0){
+    return 1;
+  }
+  if(num>0){
+    return num * factorial(--num);
+  }
 };
 
 // Do not edit below this line
