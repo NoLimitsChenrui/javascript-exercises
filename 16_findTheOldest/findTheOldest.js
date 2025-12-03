99b5f9135fbdf08810565fbd63eba62f128bc375
NoLimitsChenrui <liuchenrui2@gmail.com>
2025-12-03 16:24:30 -0500

    Complete 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const peopleAgeHierarchy = people.sort((a,b) => {
+        if(b["yearOfDeath"]){
+            return (b["yearOfDeath"] - b["yearOfBirth"]) - (a["yearOfDeath"] - a["yearOfBirth"]);
+        }
+        else{
+            return (new Date().getFullYear() - b["yearOfBirth"]) - (a["yearOfDeath"] - a["yearOfBirth"]);
+        }
+    });
+    return peopleAgeHierarchy[0];
 };
 
 // Do not edit below this line
