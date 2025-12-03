06eb98d51a7c728950c200eb24c4a754b7eb5d4c
NoLimitsChenrui <liuchenrui2@gmail.com>
2025-12-03 16:00:20 -0500

    Complete 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    if(position == 1){
+        return 1;
+    }
+    if(position == 0){
+        return 0;
+    }
+    if(position < 0){
+        return "OOPS";
+    }
+    if(position > 0){
+        return fibonacci(position-2) + fibonacci(--position);
+    }
 };
 
 // Do not edit below this line
